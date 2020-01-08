3da2a4b5d7ad1598039e18fc78b4284dcc8df353
Jingxinpo <492971588@qq.com>
2020-01-08 13:15:32 +0800

    debug

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -52,8 +52,13 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes()
     {
         Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
+
+        //添加中心管理路由
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/server.php'));
     }
 
     /**
@@ -66,8 +71,8 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
     }
 }
